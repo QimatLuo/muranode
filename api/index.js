4bf7d81e887ce0ba71da633b962363d30a2e4f6c
Qimat Luo <qimat.luo@westpharma.com>
2021-04-19 18:04:23 +0800

    service.toLowerCase()

--- api/index.js
+++ api/index.js
@@ -217,7 +217,9 @@ const Biz = (init = {}) => {
             },
             {
               get: (__, method) => (body) => {
-                let url = `${prefix}solution/${__.solutionId}/serviceconfig/${service}/call/${method}`;
+                let url = `${prefix}solution/${
+                  __.solutionId
+                }/serviceconfig/${service.toLowerCase()}/call/${method}`;
                 if (service === "user" && method === "createUserData") {
                   url = `${prefix}solution/${__.solutionId}/user/${body.id}/storage`;
                   delete body.id;
